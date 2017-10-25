5db2e880d0837183d90c610523c22629e034bc26
Greg Pfaff <greg.pfaff@razorfish.com>
2017-10-24 22:13:55 -0500

    adding updates to vue tutorial

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class='f1 code'>
+    Hello World
+    <ul>
+      <li v-for='user in users' :key='user.id'>
+        {{user.login}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import {mapState} from 'vuex'
+
+export default {
+  computed: mapState([
+    'users'
+  ])
+}
+</script>
